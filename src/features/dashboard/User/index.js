e144d8c318429b996518810f559cb1efa40b3b99
Katarzyna Napierala <k_napierala@op.pl>
2022-02-17 06:30:19 +0100

    username corrected for address in User

--- src/features/dashboard/User/index.js
+++ src/features/dashboard/User/index.js
@@ -11,7 +11,7 @@ const User = ({ id, name, username, email, address }) => {
             <td>{name}</td>
             {!!username ? <td>{username}</td> : <td></td>}
             <td>{email}</td>
-            {!!username ? <td>{address.city}</td> : <td></td>}
+            {!!address ? <td>{address.city}</td> : <td></td>}
             <td><Button title="edit" /></td>
             <td><Button title="delete" onClick={() => dispatch(removeUser(id))} /></td>
         </tr>
