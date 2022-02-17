ff56451d9eac953bd9cb27d4867ceda641bec3a9
Katarzyna Napierala <k_napierala@op.pl>
2022-02-17 07:40:34 +0100

    routing started

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-import Dashboard from './features/dashboard/Dashboard';
+import { HashRouter, Link, Switch, Route, Redirect } from "react-router-dom";
+import Dashboard from "./features/dashboard/Dashboard";
 
-const App = () => <Dashboard />;
+const App = () => (
+    <HashRouter>
+        <div>
+            <Link to="/users"></Link>
+        </div>
+
+        <Switch>
+            <Route path="/">
+                <Redirect to="/users" />
+                <Dashboard />
+            </Route>
+        </Switch>
+    </HashRouter>
+);
 
 export default App;
